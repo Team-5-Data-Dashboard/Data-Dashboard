db5131de98e5b55d789b710c847d70e21eb30034
Wayne Jones <waynejones@nyu.edu>
2020-06-13 16:57:26 -0400

    Added exclusions

--- realm/getAllShootings.js
+++ realm/getAllShootings.js
@@ -1,9 +1,12 @@
 exports = async function getAllShootings(payload, response) {
+  const exclusions = {
+    INCIDENT_KEY: 0, JURISDICTION_CODE: 0, LOCATION_DESC: 0, PERP_AGE_GROUP: 0, PERP_SEX: 0, PERP_RACE: 0, X_COORD_CD: 0, Y_COORD_CD: 0,
+  };
   const collection = context.services
     .get('mongodb-atlas')
     .db('shootings')
     .collection('shootings');
-  const shootings = await collection.find();
+  const shootings = await collection.find({}, exclusions);
 
   return shootings;
 };
