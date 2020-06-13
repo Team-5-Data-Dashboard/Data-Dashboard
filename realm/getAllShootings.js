dbfabda28bb11f11cd9ea90efee9b2fd36889598
Wayne Jones <waynejones@nyu.edu>
2020-06-13 14:35:39 -0400

    Fixed some linter issues

--- realm/getAllShootings.js
+++ realm/getAllShootings.js
@@ -1,5 +1,8 @@
-exports = async function (payload, response) {
-  const collection = context.services.get('mongodb-atlas').db('shootings').collection('shootings');
+exports = async function getAllShootings(payload, response) {
+  const collection = context.services
+    .get('mongodb-atlas')
+    .db('shootings')
+    .collection('shootings');
   const shootings = await collection.find();
 
   return shootings;
--- realm/loadData.js
+++ realm/loadData.js
@@ -1,4 +1,4 @@
-exports = function (payload, response) {
+exports = function loadData(payload, response) {
   let shootings = {};
   const result = {};
 
@@ -8,7 +8,10 @@ exports = function (payload, response) {
     console.log('Parsed Payload body: ', JSON.stringify(shootings));
 
     // Get a reference to the todos database and collection...
-    const collection = context.services.get('mongodb-atlas').db('shootings').collection('shootings');
+    const collection = context.services
+      .get('mongodb-atlas')
+      .db('shootings')
+      .collection('shootings');
 
     // Insert the new shootings...
     return collection.insertMany(shootings);
