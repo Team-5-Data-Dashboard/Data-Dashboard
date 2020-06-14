b83b6213635c77c9ef70af5816bbe3dfad587e0d
Nicholas Taylor <nicholas_taylor@me.com>
2020-06-14 13:58:31 -0400

    Just committing after straightening out some branch weirdness

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,12 +8,7 @@ function rootReducer (state = stateinit, action){
 	if (action.type === GET_ALL_SHOOTINGS){
 		const url =
 	      'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/data-dashboard-ipfkx/service/Shootings/incoming_webhook/getAllShootings';
-	    const tempObj = {...state};
-	    fetch(url)
-	      .then((res) => res.json())
-	      .then((data) => this.setState({ data }))
-	      .catch((err) => console.error('Error: ', err));
-	     
+	    const tempObj = {...state};	     
 	}
 	return state;
 }
