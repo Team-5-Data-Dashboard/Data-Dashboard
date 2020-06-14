3c615ac8011ba7e989e740bfe8583e2c4993cabd
Joshua Grossman <Josh1794@gmail.com>
2020-06-14 12:47:31 -0400

    added more to about

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Section</h1>
+      <h1>About Data Dashboard</h1>
+      <p>
+        Our team's project for the NYC Coders Black Lives Matter Hackathon was
+        to create a data dashboard for shooting incidents throughout NYC
+        utilizing NYC Open Data. We wanted to show the differences in victims
+        between communities in NYC
+      </p>
     </div>
   );
 }
