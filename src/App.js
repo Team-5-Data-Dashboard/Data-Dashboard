17cc61904d101e85ce10602e6349afa991df384e
Wayne Jones <waynejones@nyu.edu>
2020-06-14 16:19:00 -0400

    Added Color Markers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import './assets/circle.svg';
 import NavBar from './components/navBar';
 import MapComponent from './components/mapComponent';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <NavBar />
       <MapComponent />
     </div>
