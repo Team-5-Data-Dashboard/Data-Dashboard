e351eb86b972279214bccf4eef234b0ec822cb10
Joshua Grossman <Josh1794@gmail.com>
2020-06-14 13:11:51 -0400

    updated about component and added it to info panel

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function NavBar() {
-  return <div className='navbar'>Data Dashboard</div>;
+class NavBar extends Component {
+  handleSort = (e) => {
+    console.log('event', e.target.value);
+    // this.props.filterByYear(e.target.value);
+  };
+  render() {
+    console.log('navbar', this.props);
+    return (
+      <div className='navbar'>
+        Data Dashboard{' '}
+        <select onChange={this.handleSort}>
+          <option value='2006'> 2006 </option>
+          <option value='2007'> 2007 </option>
+          <option value='2008'> 2008 </option>
+          <option value='2009'> 2009 </option>
+          <option value='2010'> 2010 </option>
+          <option value='2011'> 2011 </option>
+          <option value='2012'> 2012 </option>
+          <option value='2013'> 2013 </option>
+          <option value='2014'> 2014 </option>
+          <option value='2015'> 2015 </option>
+          <option value='2016'> 2016 </option>
+          <option value='2017'> 2017 </option>
+          <option value='2018'> 2018 </option>
+          <option value='2019'> 2019 </option>
+        </select>{' '}
+      </div>
+    );
+  }
 }
 
 export default NavBar;
