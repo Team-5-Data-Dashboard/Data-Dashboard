b278c9fd2971dcaab289d0d48cb9681e0768728a
Wayne Jones <waynejones@nyu.edu>
2020-06-14 13:52:26 -0400

    Switched fetch API link to getAllByYear

--- src/components/paneMap.js
+++ src/components/paneMap.js
@@ -17,7 +17,8 @@ class PaneMap extends Component {
 
   componentDidMount() {
     const { year } = this.props;
-    const url = `https://webhooks.mongodb-stitch.com/api/client/v2.0/app/data-dashboard-ipfkx/service/Shootings/incoming_webhook/getAllShootings?year=${year}`;
+    console.log(year);
+    const url = `https://webhooks.mongodb-stitch.com/api/client/v2.0/app/data-dashboard-ipfkx/service/Shootings/incoming_webhook/getAllByYear?year=${year}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => this.setState({ data }))
