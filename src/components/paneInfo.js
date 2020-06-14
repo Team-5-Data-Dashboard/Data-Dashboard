5f3a9578a9a0ee2cd248caa24d85a5624396f6df
Wayne Jones <waynejones@nyu.edu>
2020-06-14 13:16:25 -0400

    Created props for year

--- src/components/paneInfo.js
+++ src/components/paneInfo.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import ShootingRacialChart from './shootingRacialChart';
 
-function PaneInfo() {
+function PaneInfo(props) {
+  const { year } = this.props;
   return (
-    <div className='info'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+    <div className="info">
+      <ShootingRacialChart year={2018} />
     </div>
   );
 }
